3125a6314827915dd34d0ca825c3e2f09f8589e8
Amonullo03 <133983360+Amonullo03@users.noreply.github.com>
2023-06-23 21:35:34 +0500

    message

---
+++ MainApp/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using Domain.Models;
+using Infrastructure.Services;
+var empService = new EmployeeService();
+
+var emp1 = new Employee
+{
+    FirstName = "Farzin",
+    LastName = "Parvizov",
+    Salary = 15000,
+    BirthDate = new DateTime(2001, 10, 28),
+    Department = new Department
+    {
+        Name = "IT",
+        Description = "FullStack"
+    }
+};
+
+var emp2 = new Employee
+{
+    FirstName = "Muhammad",
+    LastName = "Oev",
+    Salary = 10000,
+    BirthDate = new DateTime(2002, 11, 28),
+    Department = new Department
+    {
+        Name = "Docter",
+        Description = "***"
+    }
+};
+
+
+
+empService.AddEmployee(emp1);
+empService.AddEmployee(emp2);
+
+foreach (var employee in empService.GetEmployees())
+{
+
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Number of Id: {employee.Id} ");
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Fullname: {employee.FirstName} {employee.LastName}");
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Salary: {employee.Salary}");
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Birthday: {employee.BirthDate.ToShortDateString()}");
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Departament name: {employee.Department.Name + " " + employee.Department.Description}");
+    System.Console.WriteLine();
+    System.Console.WriteLine("_____________________________________________________________________");
+    System.Console.WriteLine();
+}
+
